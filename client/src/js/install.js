a7acd84b5a5ba7f65f308bf80741577f231831ff
Neta2393 <benetheahardin@yahoo.com>
2023-08-17 13:06:07 -0700

    Added code to JS files

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,34 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+let deferredPrompt; // To store the event for later use
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Event handler for the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the default prompt
+  event.preventDefault();
+  // Store the event for later use
+  deferredPrompt = event;
+  // Show the install button
+  butInstall.style.display = 'block';
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Event handler for the `butInstall` button click
+butInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    // Show the native install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond
+    const result = await deferredPrompt.userChoice;
+    // Reset the deferredPrompt
+    deferredPrompt = null;
+    // Hide the install button
+    butInstall.style.display = 'none';
+  }
+});
+
+// Event handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  // App was successfully installed
+  console.log('App was successfully installed.');
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,27 +3,68 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['main'],
+      }),
+
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Offline and Online Text Editor',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        start_url: '/',
+        publicPath: "/",
+        fingerprints: false,
+        inject: true,
+        icons: [
+          {
+            src: path.resolve('src/images/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js', // Path to your service worker source file
+        swDest: 'service-worker.js', // Output service worker file
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins:[
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime", 
+              ],
+            },
+          },
+        },
       ],
     },
   };
