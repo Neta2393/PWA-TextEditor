1dc99f06c8ecf638ef6129ef84a2daf3f764c044
Neta2393 <benetheahardin@yahoo.com>
2023-08-17 18:35:48 -0700

    Added engines

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies'); // Import StaleWhileRevalidate strategy
+const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -26,22 +26,5 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// Asset caching strategy using StaleWhileRevalidate
-const assetCache = new StaleWhileRevalidate({
-  cacheName: 'asset-cache', // Define a cache name for assets
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxEntries: 50, // Limit the number of entries
-      maxAgeSeconds: 7 * 24 * 60 * 60, // Cache for 7 days
-    }),
-  ],
-});
-
-// Register a route for caching assets
-registerRoute(
-  ({ request }) => request.destination === 'style' || request.destination === 'script' || request.destination === 'image',
-  assetCache
-);
+// TODO: Implement asset caching
+registerRoute();
