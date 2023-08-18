fcc85fcd9600352054b9597a016669e8160ae0e2
Neta2393 <benetheahardin@yahoo.com>
2023-08-17 19:49:38 -0700

    Changed route for image

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,8 +4,8 @@ const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 const WorkboxPlugin = require("workbox-webpack-plugin");
 
-// Added and configured workbox plugins for a service worker and manifest file
-// Added CSS loaders and babel to webpack.
+// DONE: Add and configure workbox plugins for a service worker and manifest file
+// DONE: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
