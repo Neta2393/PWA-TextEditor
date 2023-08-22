ca81d93eb4fa97dbd987b3f68afea043c895c2b6
Neta2393 <benetheahardin@yahoo.com>
2023-08-21 17:25:50 -0700

    changed routes that were misspelled

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -28,14 +28,14 @@ module.exports = () => {
       }),
       new InjectManifest({
         swSrc: "./src-sw.js",
-        swDest: "service-worker.js",
+        swDest: "src-sw.js",
       }),
       new WebpackPwaManifest({
         name: "Just Another Text Editor",
         short_name: "JATE",
         description: "A text editor for online and offline use.",
-        background_color: "#7eb4e2",
-        theme_color: "#7eb4e2",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
         start_url: "/",
         publicPath: "/",
         fingerprints: false,
